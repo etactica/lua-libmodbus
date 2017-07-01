3f46f94b39bae754ff10085182ee1818bd9a2e11
Dirk Chang <dirk@kooiot.com>
2017-07-01 18:13:02 +0800

    lua5.3 compat

--- compat.h
+++ compat.h
@@ -2,5 +2,7 @@
 #if LUA_VERSION_NUM < 502
 # define luaL_newlib(L,l) (lua_newtable(L), luaL_register(L,NULL,l))
 # define luaL_setfuncs(L,l,n) (assert(n==0), luaL_register(L,NULL,l))
+#else
+# define lua_objlen lua_rawlen
 #endif
 
